e632d6e0039e2b7c488a515f3d73de06ec6be6c8
Sanmi Ajanaku <sanmiayotunde@gmail.com>
2025-07-29 09:44:47 -0400

    fix: resolved the issue of the bible study form not submitting
    Now the form can submit

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    serverComponentsExternalPackages: ['nodemailer'],
-  },
+  // experimental: {
+    serverExternalPackages: ['nodemailer'],
+  // },
   images: {
     remotePatterns: [
       {
